705a02c80f02a6eee01463f976c88775fc62d2bb
Konrad Budukiewicz <konrad.budukiewicz@salesmore.pl>
2021-09-08 17:52:16 +0200

    update task

---
+++ project.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+FILE = '/Users/konradbudukiewicz/Downloads/green_tripdata_2019-05.csv'
+
+
+def read_data(file: str) -> pd.DataFrame:
+    """Get data from csv file.
+
+    Args:
+        file: name of the file
+    Returns
+        Dataframe with use columns.
+    """
+    df = pd.read_csv(file, usecols=['lpep_pickup_datetime', 'lpep_dropoff_datetime', 'trip_distance'])
+
+    return df
+
+
+def operations(file: str) -> pd.DataFrame:
+    """
+
+    :param file:
+    :return:
+    """
+    df = read_data(file)
+    df['lpep_pickup_datetime'] = pd.to_datetime(df['lpep_pickup_datetime'])
+    df['lpep_dropoff_datetime'] = pd.to_datetime(df['lpep_dropoff_datetime'])
+
+    df['time_lpep'] = (df['lpep_dropoff_datetime'] - df['lpep_pickup_datetime']).dt.total_seconds() / 3600
+    df['speed'] = df['trip_distance'] / df['time_lpep']
+
+    return df
+
+
+print(operations(FILE))
