ab9ac8eddd23f6b7751155b7c6c2a684ffc5e838
Konrad Budukiewicz <konrad.budukiewicz@salesmore.pl>
2021-09-11 17:51:12 +0200

    to check

--- test_spark.py
+++ test_spark.py
@@ -5,24 +5,24 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import to_timestamp
 
 
-def get_data_s3(spark_session: SparkSession, bucket: str):
-    """Get data from S3 bucket.
+# def get_data_s3(spark_session: SparkSession, bucket: str):
+#     """Get data from S3 bucket.
+#
+#     Args:
+#         spark_session: SparkSession
+#         bucket: name of the bucket in AWS S3
+#     Returns:
+#          Dataframe with data from AWS s3.
+#     """
+#     df = (
+#         spark_session.read.format("csv")
+#         .options(header="true", inferSchema="true")
+#         .load(bucket)
+#     )
+#     return df
 
-    Args:
-        spark_session: SparkSession
-        bucket: name of the bucket in AWS S3
-    Returns:
-         Dataframe with data from AWS s3.
-    """
-    df = (
-        spark_session.read.format("csv")
-        .options(header="true", inferSchema="true")
-        .load(bucket)
-    )
-    return df
 
-
-def operations_df(spark_session: SparkSession, bucket: str):
+def operations_df(df):
     """Preparing dataframe to get average speed of the month.
 
     Args:
@@ -31,7 +31,7 @@ def operations_df(spark_session: SparkSession, bucket: str):
     Returns:
         Dataframe with preparing data.
     """
-    df = get_data_s3(spark_session=spark_session, bucket=bucket)
+    # df = get_data_s3(spark_session=spark_session, bucket=bucket)
     df = df.select("lpep_pickup_datetime", "lpep_dropoff_datetime", "trip_distance")
 
     df.withColumn("timestamp", to_timestamp("lpep_pickup_datetime"))
@@ -47,36 +47,37 @@ def operations_df(spark_session: SparkSession, bucket: str):
     return df
 
 
-def average_speed(spark_session: SparkSession, bucket: str):
+def average_speed(df):
     """Get average speed of the month.
 
     Args:
-        spark_session: SparkSession
-        bucket: name of the bucket in AWS S3
+        # spark_session: SparkSession
+        # bucket: name of the bucket in AWS S3
     Returns:
         Average speed of the month.
     """
-    df = operations_df(spark_session=spark_session, bucket=bucket)
-    avg = df.agg({"trip_distance": "sum"}) / df.agg({"time_lpep": "sum"})
+    d = operations_df(df)
+    avg = d.agg({"trip_distance": "sum"}) / d.agg({"time_lpep": "sum"})
 
     return avg
 
 
 if __name__ == "__main__":
 
-    session =  SparkSession.builder.appName("Python Spark SQL basic example").getOrCreate()
+    session = SparkSession.builder.appName("Python Spark SQL basic example").getOrCreate()
     sc = session.sparkContext
     sc.setLogLevel('ERROR')
     dataFrameReader = session.read
 
-
     BUCKET = "s3://daneaseied/dane/green_tripdata_2020-05.csv"
 
-    responses = dataFrameReader.option("header", "true").csv(BUCKET)
+    data = dataFrameReader.option("header", "true").csv(BUCKET)
 
-    print("WYNIK:")
+    average = average_speed(data)
 
-    responses.printSchema()
+    print("WYNIK:", average)
+
+
 
 
 
